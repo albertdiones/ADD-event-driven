a116afd69663e34cdf5a6d0d77c0165798ad35d6
Albert Diones <adiones@addteam.org>
2025-07-03 21:18:58 +0800

    Revert "stash"
    This reverts commit 2f6b376a2263e7c05b1f871e091f45ffd11ea68c.

--- index.ts
+++ index.ts
@@ -1,43 +1,15 @@
 import { Event, EventContainer as OriginalEventContainer } from "add_event_container";
 import Logger, { LoggerInterface } from "add_logger";
 
-interface MongoLog {
-    eventType: string;
-    handlerName: string;
-    eventDate: Date,
-    executionDate: Date;
-    finishDate: Date;
-    save(): Promise<this>;
-}
-
 export class EventContainer extends OriginalEventContainer {
 
 
     logger?: LoggerInterface;
-    mongoLog?: MongoLog;
 
-    constructor(options?: { 
-            logger?: LoggerInterface,
-            mongoLog?: MongoLog 
-        }
-    ) {
+    constructor(options?: { logger: LoggerInterface }) {
         super();
         this.logger = options?.logger;
-        this.mongoLog = options?.mongoLog;
     }
-    
-    override _executeEventHandlers(event: Event, handlers: EventHandler[]): void {
-        const eventDate = new Date();
-        handlers.map(
-            (handler: EventHandler) => {
-                const executionDate = new Date();
-                handler(event) // should pass event details/context
-                const finishDate = new Date();
-                new [this.mongoLog]()
-            }
-        );
-    }
-
     override dispatchEvent(event: Event): void {
         this.logger?.info( "Event dispatched: ", event.type );
 
